7322b79700befefc2f17f7df279eec40b479cbf9
Chaser-254 <81488755+Chaser-254@users.noreply.github.com>
2024-05-10 13:16:56 +0300

    Update completed.component.ts

--- src/app/completed/completed.component.ts
+++ src/app/completed/completed.component.ts
@@ -3,23 +3,6 @@ import { ApiService } from '../api.service';
 import { DescriptionComponent } from '../description/description.component';
 import {MatDialog} from '@angular/material/dialog';
 
-/**
- * @title Completed tasks.
- * @desc this component displays the completed tasks data to on screen it adds functions
- * for the user like:
- *     - onDelete() - allows the user delete a post
- *     - openDialog() -  allows the user view the note on a post.
- *     - onUnComplete() - allows the user mark a task as uncompleted and moves it back to current tasks in view.
- *
- * @note this component exports the 'note data' from a selected task to the note.component
- * to be displayed on screen in a pop up dialog box.
- * Used imports:
- *    - import { MatDialog }
- *    - import { NoteComponent }
- *
- * Tasks are displayed from the server where they are stored.
- */
-
 @Component({
     selector: 'app-completed',
     templateUrl: './completed.component.html',
@@ -27,57 +10,39 @@ import {MatDialog} from '@angular/material/dialog';
 })
 export class CompletedComponent implements OnInit {
 
-    tasks: any = []; // Array used for locally storing tasks
+    tasks: any = [];
 
     constructor(private api: ApiService, public dialog: MatDialog) {
     }
-
-    /**
-     * @title Delete Task
-     * @desc makes a delete request to the server for a selected task
-     */
     onDelete(id: string) {
         console.log('Deleting item');
         this.api.deleteTask(id).subscribe(() => {
-            this.ngOnInit(); // Refresh the page
+            this.ngOnInit();
         });
-    }// End onDelete function
+    }
 
-    /**
-     * @title Note Dialog
-     * @desc Function used to open up a popout dialog box to display the note from 'note.component' of the
-     * task selected in the view.html.
-     * Share data with your dialog, you can use the data option to pass information to the dialog component.
-     */
     openDialog(id: String, currNote: String): void {
         const dialogRef = this.dialog.open(DescriptionComponent, {
-            data: { // passing tasks note data and id into the dialog box.
+            data: {
                 taskId: id,
                 note: currNote
-            }// End data
-        }); // end function
+            }
+        });
 
         dialogRef.afterClosed().subscribe(() => {
             console.log('The dialog was closed');
         });
-    } // End function
-
-    /**
-     * @title Mark task uncompleted
-     * @desc sets a selected task to be marked as uncompleted.
-     * @note server handles request task id and true is passed into the .updateTask();
-     * isComplete = false
-     */
+    }
     onUnComplete(id: String) {
-        this.api.updateTask(id, false).subscribe(() => { // Update selected task to 'isComplete = false'
+        this.api.updateTask(id, false).subscribe(() => {
             this.ngOnInit();
         });
-    }// End Function
+    }
 
     ngOnInit() {
         this.api.getTasksData(true).subscribe((data: any) => {
-            this.tasks = data; // get api json data for list
+            this.tasks = data;
         });
 
-    }// End Function
-}// End Class
+    }
+}
