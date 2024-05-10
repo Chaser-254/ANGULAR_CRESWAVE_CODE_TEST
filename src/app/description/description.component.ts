0520df8f8112a921491954367b2d144010534b58
Chaser-254 <81488755+Chaser-254@users.noreply.github.com>
2024-05-10 13:23:48 +0300

    Update description.component.ts

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -3,16 +3,6 @@ import {DialogData} from '../view/view.component';
 import { ApiService } from '../api.service';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 
-/**
- * @title View Notes
- * @desc this component displays the note for a selected task.
- * @note this component reads in note data from 'view.component' it is read in from
- * imported interface:
- *     - import { DialogData }
- * With the usage of:
- *     - import { MatDialog, MatDialogRef, MAT_DIALOG_DATA }
- */
-
 @Component({
     selector: 'app-note',
     templateUrl: './description.component.html',
@@ -23,46 +13,25 @@ export class NoteComponent implements OnInit {
     constructor(public dialogRef: MatDialogRef<NoteComponent>,
                 @Inject(MAT_DIALOG_DATA) public tasksData: DialogData, private api: ApiService) {
     }
-
-    /**
-     * @title Closes NoteComponent dialog.
-     * @desc closes the dialog box if clicked outside the box.
-     */
     onNoClick(): void {
         this.dialogRef.close();
     }
-
-    /**
-     * @title Updates a tasks note.
-     * @desc updates a tasks note after a user edits the text box and submits it.
-     * it passes the values into the post servers updateNote() to be then put onto the server.
-     */
     onUpdate(updatedNote: String) {
         this.api.updateNote(this.tasksData.taskId, updatedNote).subscribe(data => {
-            this.tasksData = data; // Update tasks data with the new data
-            this.dialogRef.close(); // Close the dialog box
-            location.reload();  // Reload the webpage
+            this.tasksData = data;
+            this.dialogRef.close();
+            location.reload();
         });
-    } // End onUpdate() function
+    }
 
     ngOnInit() {
-    } // End function
+    }
 
 } // End Class
 import {Component, OnInit, Inject} from '@angular/core';
 import {DialogData} from '../view/view.component';
 import {ApiService} from '../api.service';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-
-/**
- * @title View Notes
- * @desc this component displays the note for a selected task.
- * @note this component reads in note data from 'view.component' it is read in from
- * imported interface:
- *     - import { DialogData }
- * With the usage of:
- *     - import { MatDialog, MatDialogRef, MAT_DIALOG_DATA }
- */
 
 @Component({
     selector: 'app-note',
@@ -74,29 +43,18 @@ export class DescriptionComponent implements OnInit {
     constructor(public dialogRef: MatDialogRef<NoteComponent>,
                 @Inject(MAT_DIALOG_DATA) public tasksData: DialogData, private api: ApiService) {
     }
-
-    /**
-     * @title Closes NoteComponent dialog.
-     * @desc closes the dialog box if clicked outside the box.
-     */
     onNoClick(): void {
         this.dialogRef.close();
     }
-
-    /**
-     * @title Updates a tasks note.
-     * @desc updates a tasks note after a user edits the text box and submits it.
-     * it passes the values into the post servers updateNote() to be then put onto the server.
-     */
     onUpdate(updatedNote: String) {
         this.api.updateNote(this.tasksData.taskId, updatedNote).subscribe(data => {
-            this.tasksData = data; // Update tasks data with the new data
-            this.dialogRef.close(); // Close the dialog box
-            location.reload();  // Reload the webpage
+            this.tasksData = data;
+            this.dialogRef.close();
+            location.reload();
         });
-    } // End onUpdate() function
+    }
 
     ngOnInit() {
-    } // End function
+    }
 
-} // End Class
+}
