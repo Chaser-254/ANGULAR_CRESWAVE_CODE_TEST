2144a6c115bde3b9e42b83b0466e166cb4fbda4c
Chaser-254 <81488755+Chaser-254@users.noreply.github.com>
2024-05-10 13:24:48 +0300

    Update edit.component.ts

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -3,18 +3,6 @@ import {Router, ActivatedRoute} from '@angular/router';
 import {ApiService} from '../api.service';
 import {NgForm} from '@angular/forms';
 
-/**
- * @title Edit tasks.
- * @desc this component allows the user update a selected task.
- * Functions:
- *     - onDelete() - allows the user delete a post
- *     - openDialog() -  allows the user view the note on a post.
- *     - onComplete() - allows the user mark a task as complete.
- *     - onUpdate() - allows the user update the priority of a task.
- *
- * Tasks are pulled from the server where it is stored.
- */
-
 @Component({
     selector: 'app-edit',
     templateUrl: './edit.component.html',
@@ -27,21 +15,15 @@ export class EditComponent implements OnInit {
     constructor(private router: Router, private route: ActivatedRoute, private api: ApiService) {
     }
 
-    /**
-     * @title Edit post
-     * @desc Function used edit a selected post.
-     * Used imports:
-     *      - import { NgForm }
-     */
     onEditPost(form: NgForm) {
         this.api.editTask(this.task[0]._id, form.value.task_name, form.value.note, form.value.priority, form.value.date, false).subscribe();
-        this.router.navigate(['/']); // Return the home
-    }// End edit post
+        this.router.navigate(['/']);
+    }
 
     ngOnInit() {
         console.log(this.route.snapshot.params['id']);
         this.api.getTask(this.route.snapshot.params['id']).subscribe(data => {
-            this.task = data; // API JSON data received from the server passed into tasks array
+            this.task = data;
         });
-    } // End ngOnInit
-}// End class
+    }
+}
